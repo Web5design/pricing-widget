736d15f010af3c5f83f906e7350daa23e4b648f4
James Halliday <mail@substack.net>
2013-01-10 12:42:29 -0800

    more and features navigation

--- example/plans.js
+++ example/plans.js
@@ -9,19 +9,31 @@ plans.add('free', {
     price: 0,
     per: 'month',
     image: 'images/free.png',
-    description: 'free plan...'
+    description: 'Great for students, casual cross-browser testing,'
+        + ' and product evaluation',
+    features: [ '5-minute sessions' ],
+    more: [
+        'Use browserling for 5 minutes at a time as much as you like.',
+        'Sometimes you\'ll need to wait in a queue if the site is busy.'
+    ]
 });
 
 plans.add('developer', {
     price: 20,
     per: 'month',
     image: 'images/developer.png',
-    description: 'developer plan...'
+    description: 'Perfect for designers, front-end developers, and freelancers'
+        + ' who need to check their work against all the browsers.',
+    features: [ 'unlimited sessions', 'ssh tunnels' ],
+    more: [
+        'Use browserling for as long as you need, as much as you like.',
+        'SSH tunnels to access localhost and intranet services'
+    ]
 });
 
 plans.add('team', {
     price: function (n) { return n * 20 },
     per: 'month',
     image: 'images/developer.png',
-    description: 'developer plan...'
+    description: ''
 });
--- index.js
+++ index.js
@@ -15,10 +15,14 @@ function Plans (cb) {
     if (!(this instanceof Plans)) return new Plans(cb);
     EventEmitter.call(this);
     
+    this.element = hyperglue(html.box, {});
+    this.plans = document.createElement('div');
+    
     this.pages = swoop({
-        plans: hyperglue(html.box, {})
+        plans: this.plans
     });
     this.pages.show('plans');
+    this.pages.appendTo(this.element);
     
     if (typeof cb === 'function') this.on('buy', cb);
 }
@@ -26,19 +30,41 @@ function Plans (cb) {
 Plans.prototype = new EventEmitter;
 
 Plans.prototype.add = function (name, plan) {
-    var div = hyperglue(html.plan, {
+    var self = this;
+    var more = document.createElement('ul');
+    var morePoints = plan.more || [];
+    for (var i = 0; i < morePoints.length; i++) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(morePoints[i]));
+        more.appendChild(li);
+    }
+    
+    var params = {
         '.icon img': { src: plan.image },
         '.price .amount': plan.price,
         '.price .per': plan.per ? '/ ' + plan.per : '',
         '.title': plan.title !== undefined ? plan.title : name + ' plan',
         '.desc .text': plan.description || '',
+        '.features': (plan.features || []).join(', '),
+        '.more': more,
+    };
+    var label = hyperglue(html.plan, params);
+    more.style.display = 'none';
+    
+    label.addEventListener('click', function (ev) {
+        label.style.display = 'block';
+        self.pages.show('_' + name);
     });
-    this.pages.slides.plans.appendChild(div);
+    self.plans.appendChild(label);
+    
+    var slide = label.cloneNode(true);
+    slide.querySelector('.more').style.display = 'block';
+    self.pages.addSlide('_' + name, slide);
 };
 
 Plans.prototype.appendTo = function (target) {
     if (typeof target === 'string') {
         target = document.querySelector(target);
     }
-    this.pages.appendTo(target);
+    target.appendChild(this.element);
 };
